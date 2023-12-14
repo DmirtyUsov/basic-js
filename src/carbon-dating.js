b0059e612a49ad21a11baa2652f3a53a6343aa98
Dima Usov <dmitry.usov@yahoo.com>
2023-12-14 23:02:32 +0300

    feat: add carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const activity = parseFloat(sampleActivity)
+  return typeof sampleActivity === 'string'
+    && activity
+    && activity > 0
+    && activity < MODERN_ACTIVITY
+    ? Math.ceil(Math.log(MODERN_ACTIVITY / activity) / (0.693 / HALF_LIFE_PERIOD))
+    : false;
 }
 
 module.exports = {
