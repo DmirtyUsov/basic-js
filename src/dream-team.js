2ebdd914b700336fbd65e34bb72d9bff134f5546
Dima Usov <dmitry.usov@yahoo.com>
2023-12-14 23:23:57 +0300

    feat: add dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  return Array.isArray(members)
+    ? members.reduce((team, name) => {
+      const newMember = typeof name === 'string' ? name.trimStart().charAt(0).toUpperCase() : '';
+      return team.concat(newMember);
+      },'')
+      .split('')
+      .sort()
+      .join('')
+    : false;
 }
 
 module.exports = {
