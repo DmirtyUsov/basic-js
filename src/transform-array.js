d9dcb7bdf9ea32aa8b5dcdc44e0feee8fdc7e5d8
Dima Usov <dmitry.usov@yahoo.com>
2023-12-15 23:18:52 +0300

    feat: add transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  let commandPrev ='';
+  let prevNotDiscarded = true;
+
+  if(!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  console.log(arr);
+
+  return arr.reduce((newArray, item) => {
+    if (typeof item === 'string') {
+      if (item.startsWith('--')) {
+        switch (item) {
+          case '--discard-next':
+          case '--double-next':
+            commandPrev = item;
+            return newArray;
+            break;
+          case '--double-prev':
+            if(prevNotDiscarded && newArray.length) {
+              newArray.push(newArray[newArray.length - 1]);
+            }
+            prevNotDiscarded = true;
+            return newArray;
+            break;
+          case '--discard-prev':
+            if(prevNotDiscarded) {
+              newArray.pop();
+            }
+            prevNotDiscarded = true;
+            return newArray;
+            break;
+          }
+      }
+    }
+  
+    switch (commandPrev) {
+      case '--discard-next':
+        prevNotDiscarded = false;
+        break;
+      case '--double-next':
+        newArray.push(item);
+      default:
+        if(item !== undefined) {
+          newArray.push(item);
+        }
+    }
+    commandPrev = '';
+  
+    return newArray;
+  },[])
 }
 
 module.exports = {
