34db5db917e23cff8056961bbf2f4d04a643ed2c
Dima Usov <dmitry.usov@yahoo.com>
2023-12-15 23:19:15 +0300

    feat: add simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,28 +5,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = '') {
+    this.chain.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    position -= 1; // zero index
+    if ((typeof position !== 'number')
+      || (position < 0)
+      || (position > this.getLength()-1)
+   ) {
+      this.chain = [];
+      throw new Error('You can\'t remove incorrect link!');
+    } else {
+      this.chain.splice(position, 1);
+    }
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain = this.chain.reverse()
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let result = '';
+    result = this.chain.map(link => `( ${String(link)} )`).join('~~');
+    this.chain = [];
+    return result;
   }
 };
 
+// console.log(
+//   chainMaker
+//   .addLink(3.14)
+//   .addLink(1)
+//   .addLink({ 0: 'first', 1: 'second', 'length': 2 })
+//   .removeLink(1)
+//   .addLink('DEF')
+//   .addLink({ 0: 'first', 1: 'second', 'length': 2 })
+//   .removeLink(1)
+//   .addLink(true)
+//   .addLink(false)
+//   .addLink(333)
+//   .reverseChain()
+//   .reverseChain()
+//   .finishChain()
+// )
+
+// chainMaker.addLink(1).addLink(2).addLink(3).removeLink(0),
+// chainMaker.addLink(1).addLink(2).addLink(3).removeLink('2nd'),
+// chainMaker.addLink(1).addLink(2).addLink(3).removeLink(-2),
+// chainMaker.addLink(1).addLink(2).addLink(3).removeLink(4)
+//( [object Object] )~~( DEF )~~( [object Object] )~~( true )~~( false )~~( 333 )
 module.exports = {
   chainMaker
 };
