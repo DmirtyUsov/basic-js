c1c723292f800ba0d734f9536e56724c53a6a446
Dima Usov <dmitry.usov@yahoo.com>
2023-12-17 17:14:25 +0300

    feat: add cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,68 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  isDirect;
+  startAlphabet = 65; // Letter A
+
+  constructor(isDirect=true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if(!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const chars = message.toUpperCase().split('');
+    let encryptedMessage = '';
+    const newChars = this.shift(chars, key);
+    if(this.isDirect)
+    {
+      encryptedMessage = newChars.join('');
+    } else {
+      encryptedMessage = newChars.reverse().join('');
+    }
+    return encryptedMessage;
+  }
+  decrypt(encryptedMessage, key) {
+    if(!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    const chars = encryptedMessage.toUpperCase().split('');
+    let message = '';
+    const newChars = this.shift(chars, key, false);
+    if(this.isDirect) {
+      message = newChars.join('');
+    } else {
+      message = newChars.reverse().join('');
+    }
+    return message;
+  }
+
+  createShifts(key) {
+    return key
+      .toUpperCase()
+      .split('')
+      .map(char => char.charCodeAt(0) - this.startAlphabet);
+  }
+  shift(chars, key, encrypt=true) {
+    const totalLetters = 26;
+    const shifts = this.createShifts(key);
+    let shiftDirection = encrypt ? 1 : -1;
+    let idxCorrection = 0;
+    return chars
+      .map((char,idx) => {
+        let newCharCode = -1;
+        const shiftIndex = (idx + idxCorrection) % shifts.length;
+        const charCode = char.charCodeAt(0) - this.startAlphabet;
+        if(charCode > -1 && charCode < totalLetters){
+          newCharCode = (totalLetters + charCode + shifts[shiftIndex]*shiftDirection) % totalLetters;
+        }
+        else {
+          newCharCode = charCode;
+          idxCorrection += -1;
+        }
+        return String.fromCharCode(newCharCode + this.startAlphabet);
+      })
   }
 }
 
